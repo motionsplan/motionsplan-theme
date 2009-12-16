f05a342588eb6924f237b317125e44205905f2ee
phpgeek <johan@phpgeek.dk>
2009-12-16 20:50:03 +0000

    Initial import

---
+++ node-exercise.tpl.php
@@ -0,0 +1,106 @@
+<?php
+if($page) {
+?>
+<div class="exercise node <?php print $node_classes; ?>">
+    <?php
+    if($node->field_alternative_titles[0]['value'] != '') {
+        $titles = array();
+        foreach($node->field_alternative_titles as $t) {
+            $titles[] = $t['value'];
+        }
+        echo '<h2 class="alternative"><strong>Alternative navne:</strong> '.implode(", ",$titles).'</h2>';
+    }
+    
+     
+    echo '<div class="intro">'.$node->field_exercise_intro[0]['safe'].'</div>';
+    echo '<h4>Sådan udføres øvelsen</h4>';
+    echo '<div class="guide">'.$node->field_exercise_guide[0]['value'].'</div>';
+    
+    
+    echo '<div style="clear:right;font-size:1px;"></div>';
+    
+    if(sizeof($node->field_exercise_videos)>0) {
+        $videoHTML = '';
+        foreach($node->field_exercise_videos as $video) {
+            if(!empty($video['view'])) {
+                $videoHTML .= '<div class="video">' . $video['view'] . '</div>';
+            }
+        }
+        if($videoHTML != '') {
+            echo '<h4>Video af øvelsen</h4>';
+        }
+        if(!empty($user->uid)) {
+            echo $videoHTML;  
+        }
+        else {
+            echo '<p>
+                Der er tilknyttet en video til denne øvelse.<br />
+                Du skal dog være 
+                <a href="/user?destination=node/'.$node->nid.'">registeret bruger</a>
+                for at se videoen.
+                </p>
+                <p>
+                <strong><a href="/user/register?destination=node/'.$node->nid.'">
+                Klik her for at registere dig som bruger GRATIS</a></strong>
+                </p>
+            ';
+        }
+        
+    }
+    
+    if($node->field_exercise_images[0]['view']!='') {
+        echo '<h4>Billeder af øvelsen</h4>';
+        foreach($node->field_exercise_images as $img) {
+            echo '<div class="exerciseimg">';
+            if($img['data']['description']!='') {
+                echo '<h5>'.$img['data']['description'].'</h5>';
+            }
+            echo $img['view'];
+            echo '</div>';
+        }
+        echo '<div style="clear:both;"></div>';
+    }
+    
+    
+    ?>
+
+  <?php if ($links): ?>
+  <div class="links">
+    <?php print $links; ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if ($node_bottom && !$teaser): ?>
+  <div id="node-bottom">
+    <?php print $node_bottom; ?>
+  </div>
+  <?php endif; ?>
+
+</div>
+<?php
+}
+else {
+?>
+<div class="exercistlist">
+    <?php
+    if(!empty($node->field_exercise_images[0]['filepath'])) {
+        $img = $node->field_exercise_images[0]['filepath'];
+        $img = str_replace('/files/','/files/imagecache/exercisePictureList/',$img);
+        echo '<div class="photo"><a href="'.url('node/'.$node->nid).'"><img src="/'.$img.'" alt="" /></a></div>';
+    }
+    ?>
+    <h2><a href="<?=url('node/'.$node->nid);?>"><?=$node->title?></a></h2>
+    <?=$node->field_exercise_intro[0]['value']?>
+    <br />
+    <?php echo l('Vis '.$node->title,'node/'.$node->nid); ?>
+    <?php
+    if(arg(0) == 'exerciseprogram' && arg(1) == 'show') {
+        $dURL = url('exerciseprogram/show/'.arg(2).'/delete/'.$node->nid);
+	echo '| <a href="javascript:void(0)" onclick="if(confirm(\'Er du sikker på du vil slette øvelsen?\'))location.href=\''.$dURL.'\'" />Slet øvelse fra program</a>';
+    }
+    ?>
+    <hr />
+</div>
+<?php
+}
+?>
